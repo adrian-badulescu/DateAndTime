4ddec00c40da400b7b4d0d5987a8d1d02d95ffc9
adrianb-badulescu <adrianb2104@gmail.com>
2021-10-25 21:02:18 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DotNet
+{
+    class Program : DateAndTime
+    {
+        IDateAndTime now { get; set; }
+        static void Main(string[] args)
+        {
+            IDateAndTime now = new DateAndTime();
+            Console.WriteLine(now.getDateAndTime());
+        }
+    }
+}
+
+class DateAndTime : IDateAndTime
+{
+    public DateTime dt = DateTime.Now;
+
+    public DateTime getDateAndTime()
+    {
+        // Console.WriteLine(dt);
+        return dt;
+    }
+}
+
+interface IDateAndTime
+{
+    DateTime getDateAndTime();
+}
